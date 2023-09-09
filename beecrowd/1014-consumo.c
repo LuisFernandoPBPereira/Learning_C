b5ef9310b05a7834ea4b9388157b6a1fe570107d
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2023-09-08 23:51:04 -0300

    Adding more exercises

---
+++ beecrowd/1014-consumo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+    //x representa a distância
+    int x;
+    //y representa o combustível gasto
+    float y, consumo;
+    
+    scanf("%d%f", &x, &y);
+    
+    consumo = x/y;
+    
+    printf("%.3f km/l\n", consumo);
+ 
+    return 0;
+}
