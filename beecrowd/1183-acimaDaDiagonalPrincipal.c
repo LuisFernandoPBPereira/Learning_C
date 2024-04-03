6c585320953b3a3f19492727da361634ce958aca
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-04-03 16:13:19 -0300

    Create 1183-acimaDaDiagonalPrincipal.c

---
+++ beecrowd/1183-acimaDaDiagonalPrincipal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    char op;
+    float matriz[12][12], soma=0, cont=0;
+    
+    scanf(" %c", &op);
+ 
+    for(int i=0; i<12; i++){
+        for(int j=0; j<12; j++){
+            scanf("%f", &matriz[i][j]);
+        }
+    }
+    
+    for(int i=0; i<12; i++){
+        for(int j=i+1; j<12; j++){
+            soma+=matriz[i][j];
+            cont++;
+        }
+    }
+    
+    if(op == 'M'){
+        soma /=cont;
+    }
+    printf("%.1f\n", soma);
+ 
+    return 0;
+}
