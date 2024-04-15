a64f71d096bc73236863cac8383a70deeeb19604
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-04-15 15:52:55 -0300

    Create filaV2.c

---
+++ Estrutura de Dados 1/Fila/filaV2.c
@@ -0,0 +1,109 @@
+#include <malloc.h>
+#include <stdio.h>
+
+typedef struct no 
+{
+  int valor;
+  struct no *proximo;
+} No;
+
+typedef struct {
+  No* primeiro;
+  No* fim;
+  int tamanho;
+}Fila;
+
+void criarFila(Fila *fila){
+  fila->primeiro = NULL;
+  fila->fim = NULL;
+  fila->tamanho = 0;
+}
+
+void inserirNaFila(Fila *fila, int num) 
+{
+  No *aux, *novoItem = malloc(sizeof(No));
+
+  if (novoItem != NULL) 
+  {
+    novoItem->valor = num;
+    novoItem->proximo = NULL;
+    if (fila->primeiro == NULL) 
+    {
+      fila->primeiro = novoItem;
+      fila->fim = novoItem;
+    } 
+    else 
+    {
+      fila->fim->proximo = novoItem;
+      fila->fim = novoItem;
+    }
+      fila->tamanho++;
+  } else
+    printf("Erro ao alocar memória...\n");
+}
+
+No *removerDaFila(Fila *fila){
+  No *remover = NULL;
+
+  if(fila->primeiro != NULL){
+    remover = fila->primeiro;
+    fila->primeiro = remover->proximo;
+    fila->tamanho--;
+  }
+  else printf("\nFila vazia\n");
+
+  return remover;
+}
+
+void imprimirFila(Fila *fila){
+  No* aux = fila->primeiro;
+  printf("\t---------- FILA ---------\n");
+  while(aux != NULL){
+    printf("\t\t\t%d\n", aux->valor);
+    aux = aux->proximo;
+  }
+  printf("\n\t------ FIM DA FILA ------\n\n");
+
+}
+
+int main() { 
+  No *remover;
+  Fila fila;
+
+  criarFila(&fila);
+  
+  int opcao, valor;
+
+  do{
+    printf("0 - Sair\n1 - Inserir\n2 - Remover\n3 - Imprimir\n");
+    scanf("%d", &opcao);
+
+    switch(opcao){
+      case 0:
+        printf("Saindo...\n");
+        break;
+      case 1:
+        printf("Digite um valor:\n");
+        scanf("%d", &valor);
+        inserirNaFila(&fila, valor);
+        break;
+      case 2:
+        remover = removerDaFila(&fila);
+        if(remover != NULL){
+          printf("\nElemento removido: %d\n\n", remover->valor);
+          free(remover);
+        }
+        else{
+          printf("\nFila vazia\n");
+        }
+        break;
+      case 3:
+        imprimirFila(&fila);
+        break;
+      default:
+        printf("\nOpcao invalida\n");
+        break;
+    }
+
+  }while(opcao != 0);
+}
